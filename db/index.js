f38297afad7687ad5c942d72e2e7d85f44229a6c
Brendon Allen Curry-Hobbs <46946074+brendonc-h@users.noreply.github.com>
2022-10-10 21:26:22 -0600

    manager erase commit

--- db/index.js
+++ db/index.js
@@ -7,7 +7,7 @@ class DB {
 
   findAllEmployees() {
     return this.connection.promise().query(
-      "SELECT employee.id, employee.first_name, employee.last_name, role.title, department.department_name AS department, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager FROM employee LEFT JOIN role on employee.role_key = role.id LEFT JOIN department on role.department_key = department.id LEFT JOIN employee manager on manager.id = employee.manager_key;"
+      "SELECT employee.id, employee.first_name, employee.last_name, role.title, department.department_name AS department, role.salary, CONCAT;"
     );
   }
 
@@ -52,14 +52,14 @@ class DB {
       "SELECT employee.id, employee.first_name, employee.last_name, role.title FROM employee LEFT JOIN role on employee.role_id = role.id LEFT JOIN department department on role.department_id = department.id WHERE department.id = ?;",
       departmentId
     );
-  }
-  findAllEmployeesByManager(managerId) {
-    return this.connection.promise().query(
-      "SELECT employee.id, employee.first_name, employee.last_name, department.department_name AS department, role.title FROM employee LEFT JOIN role on role.key = employee.role_id LEFT JOIN department ON department.key = role.department_key WHERE department.manager_name = ?;",
-      managerId
-    );
-  }
-}
+  }}
+//   findAllEmployeesByManager(managerId) {
+//     return this.connection.promise().query(
+//       "SELECT employee.id, employee.first_name, employee.last_name, department.department_name AS department, role.title FROM employee LEFT JOIN role on role.key = employee.role_id LEFT JOIN department ON department.key = role.department_key WHERE department.manager_name = ?;",
+//       managerId
+//     );
+//   }
+// }
 
 module.exports = new DB(connection);
 
